912886d867820b652acdc93e734b82e97a941607
PROF. JOEL <joel.n.wanjala@gmail.com>
2022-12-01 12:31:16 -0800

    Learnt how to use console colors

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,10 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Change the console color
+            /*Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();*/
+
             // Display message to the console
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello, Joel");
-            Console.WriteLine("Goodbye world");
+            Console.WriteLine("Goodbye world");       
+
         }
     }
 }
